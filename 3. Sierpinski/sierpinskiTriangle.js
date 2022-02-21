73cce0532ce3538b1f5ceeb8c5147c1e0eca19da
Eduardo Roberto Muller Romero <a01745219@tec.mx>
2022-02-21 15:01:33 -0600

    sin console.log innecesario, limpiando el codigo pt.2

--- 3. Sierpinski/sierpinskiTriangle.js
+++ 3. Sierpinski/sierpinskiTriangle.js
@@ -36,12 +36,12 @@ function getMidPoints(pointArray){
     let v2 = pointArray[1]
     let v3 = pointArray[2]
 
-    midPointA = Array((v1[0]+v2[0])/2, (v1[1]+v2[1])/2)
+    midPointA = Array((v1[0]+v2[0])/2, (v1[1]+v2[1])/2) //Se obtienen los puntos medios de la forma Q((x1+x2)/2, (y1+y2)/2)
     midPointB = Array((v2[0]+v3[0])/2, (v2[1]+v3[1])/2)
     midPointC = Array((v1[0]+v3[0])/2, (v1[1]+v3[1])/2)
     let newPointArray = [midPointA, midPointB, midPointC]
 
-    return newPointArray
+    return newPointArray //Se regresa un arreglo con el mismo formato de pointArray pero con las nuevas coordenadas
 }
 
 function recursiveFunc(ctx, pointArr, steps){
@@ -64,18 +64,14 @@ function sliderEvent(canvas, ctx, pointArr){
         ctx.clearRect(0,0, canvas.width, canvas.height)
         recursiveFunc(ctx, pointArr, steps)
     };
-    //drawTriangle(ctx, pointArr) 
-        
 }
 
 function main(){
     let pointArr = [[200, 100],[100, 300],[300, 300]] //Coordenadas del primer triangulo
-    console.log("The original pointArray: [" + pointArr + "]")
-    // To draw something on a <canvas>, you must first retrieve the <canvas> element from the HTML file
+
     let canvas = document.getElementById("htmlCanvas");
     let ctx = canvas.getContext("2d");
 
-    // If the canvas is not null, the element was retrieved successfully.
     if(!canvas)
     {
         console.log("Failed to load the canvas element.")
@@ -87,17 +83,5 @@ function main(){
     }
 
     sliderEvent(canvas, ctx, pointArr)
-
-    //Ejemplo de como se dibuja
-    /*ctx.beginPath()
-    ctx.moveTo(200, 100)
-    ctx.lineTo(100, 300)
-    ctx.lineTo(300, 300)
-    ctx.closePath()
-    ctx.stroke()*/
-
-    //drawTriangle(ctx, pointArr) //Testing function
-
-
 
 }
