ec1ac6259f868ccc3c0476f9ff07fbcb9566dbbf
Eduardo Roberto Muller Romero <a01745219@tec.mx>
2022-03-23 19:26:27 -0600

    primer examen

---
+++ Examen1/pyramid.js
@@ -0,0 +1,277 @@
+let projectionMatrix = null, shaderProgram = null;
+
+let shaderVertexPositionAttribute = null, shaderVertexColorAttribute = null, shaderProjectionMatrixUniform = null, shaderModelViewMatrixUniform = null;
+
+let mat4 = glMatrix.mat4;
+
+let duration = 10000;
+
+let vertexShaderSource = `#version 300 es
+in vec3 vertexPos;
+in vec4 vertexColor;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+out vec4 vColor;
+
+void main(void) {
+    // Return the transformed and projected vertex value
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPos, 1.0);
+    // Output the vertexColor in vColor
+    vColor = vertexColor * 0.75;
+}`;
+
+let fragmentShaderSource = `#version 300 es
+    precision lowp float;
+    in vec4 vColor;
+    out vec4 fragColor;
+
+    void main(void) {
+    // Return the pixel color: always output white
+    fragColor = vColor;
+}
+`;
+
+function createShader(glCtx, str, type)
+{
+    let shader = null;
+    
+    if (type == "fragment") 
+        shader = glCtx.createShader(glCtx.FRAGMENT_SHADER);
+    else if (type == "vertex")
+        shader = glCtx.createShader(glCtx.VERTEX_SHADER);
+    else
+        return null;
+
+    glCtx.shaderSource(shader, str);
+    glCtx.compileShader(shader);
+
+    if (!glCtx.getShaderParameter(shader, glCtx.COMPILE_STATUS)) {
+        throw new Error(glCtx.getShaderInfoLog(shader));
+    }
+
+    return shader;
+}
+
+function initShader(glCtx, vertexShaderSource, fragmentShaderSource)
+{
+    const vertexShader = createShader(glCtx, vertexShaderSource, "vertex");
+    const fragmentShader = createShader(glCtx, fragmentShaderSource, "fragment");
+
+    let shaderProgram = glCtx.createProgram();
+
+    glCtx.attachShader(shaderProgram, vertexShader);
+    glCtx.attachShader(shaderProgram, fragmentShader);
+    glCtx.linkProgram(shaderProgram);
+    
+    if (!glCtx.getProgramParameter(shaderProgram, glCtx.LINK_STATUS)) {
+        throw new Error("Could not initialise shaders");
+    }
+
+    return shaderProgram;
+}
+
+function initWebGL(canvas) 
+{
+    let gl = null;
+    let msg = "Your browser does not support WebGL, or it is not enabled by default.";
+
+    try 
+    {
+        gl = canvas.getContext("webgl2");
+    } 
+    catch (e)
+    {
+        msg = "Error creating WebGL Context!: " + e.toString();
+    }
+
+    if (!gl)
+    {
+        throw new Error(msg);
+    }
+
+    return gl;        
+}
+
+function initViewport(gl, canvas)
+{
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+function initGL(gl, canvas)
+{
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 1, 100);
+}
+
+function draw(gl, objs) 
+{
+    // clear the background (with black)
+    gl.clearColor(0, 0, 0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // set the shader to use
+    gl.useProgram(shaderProgram);
+
+    for(const obj of objs)
+    {
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.buffer);
+        gl.vertexAttribPointer(shaderVertexPositionAttribute, obj.vertSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.colorBuffer);
+        gl.vertexAttribPointer(shaderVertexColorAttribute, obj.colorSize, gl.FLOAT, false, 0, 0);
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indices);
+
+        gl.uniformMatrix4fv(shaderProjectionMatrixUniform, false, projectionMatrix);
+        gl.uniformMatrix4fv(shaderModelViewMatrixUniform, false, obj.modelViewMatrix);
+
+        gl.drawElements(obj.primtype, obj.nIndices, gl.UNSIGNED_SHORT, 0);
+    }
+}
+
+
+function createPyramid(gl, translation, rotationAxis){
+
+    let vertArray = []
+
+    Sierpinski([-1, 0, -1],[-1, 0, 1],[0, 1, 0],vertArray,3)
+    Sierpinski([1, 0, 0],[0, 1, 0],[-1, 0, -1],vertArray,3)
+    Sierpinski([1, 0, 0],[-1, 0, 1],[0, 1, 0],vertArray,3)
+    Sierpinski([1, 0, 0],[-1, 0, -1],[-1, 0, 1],vertArray,3)
+
+
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertArray), gl.STATIC_DRAW);
+
+    let colorArray = []
+    for (var i = 0; i < 108; i++) {
+        let r = Math.random() //random so each can be different
+        let g = Math.random()
+        let b = Math.random()
+        let a = 1
+        let rgbaArray = [r, g, b, a]
+        colorArray.push(rgbaArray)
+    }
+
+    let vertexColors = [];
+    colorArray.forEach(color => {
+        for (var i = 0; i < 3; i++) {
+            vertexColors.push(...color)
+        }
+    })
+
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+    // Index data (defines the triangles to be drawn).
+    let pyramidIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndexBuffer);
+
+    let pyramidIndexArray = []
+    for (var i = 0; i < 108; i++) {
+        pyramidIndexArray.push(i)
+    }
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidIndexArray), gl.STATIC_DRAW);
+
+    let pyramid = {
+        buffer: vertexBuffer, colorBuffer: colorBuffer, indices: pyramidIndexBuffer,
+        vertSize: 3, nVerts: vertArray.length / 3, colorSize: 4, nColors: vertexColors.length / 4, nIndices: pyramidIndexArray.length,
+        primtype: gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime: Date.now()
+    };
+
+    mat4.translate(pyramid.modelViewMatrix, pyramid.modelViewMatrix, translation);
+    mat4.rotate(pyramid.modelViewMatrix, pyramid.modelViewMatrix, Math.PI / 8, [1, 0, 0]);
+
+    pyramid.update = function () {
+        let now = Date.now();
+        let deltat = now - this.currentTime;
+        this.currentTime = now;
+        let fract = deltat / duration;
+        let angle = Math.PI * 2 * fract;
+
+        mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+    };
+
+    return pyramid;
+}
+
+function update(glCtx, objs)
+{
+    requestAnimationFrame(()=>update(glCtx, objs));
+
+    draw(glCtx, objs);
+    objs.forEach(obj => obj.update())
+}
+
+function bindShaderAttributes(glCtx, shaderProgram)
+{
+    shaderVertexPositionAttribute = glCtx.getAttribLocation(shaderProgram, "vertexPos");
+    glCtx.enableVertexAttribArray(shaderVertexPositionAttribute);
+
+    shaderVertexColorAttribute = glCtx.getAttribLocation(shaderProgram, "vertexColor");
+    glCtx.enableVertexAttribArray(shaderVertexColorAttribute);
+    
+    shaderProjectionMatrixUniform = glCtx.getUniformLocation(shaderProgram, "projectionMatrix");
+    shaderModelViewMatrixUniform = glCtx.getUniformLocation(shaderProgram, "modelViewMatrix");
+}
+
+function Sierpinski(vertex1, vertex2, vertex3, vertexArray, step) {
+
+    if (step <= 1) {
+        vertexArray.push(vertex1[0], vertex1[1], vertex1[2],
+            vertex2[0], vertex2[1], vertex2[2], 
+            vertex3[0], vertex3[1], vertex3[2])
+    }
+    else {
+
+        let x1 = (vertex2[0] + vertex3[0]) / 2
+        let y1 = (vertex2[1] + vertex3[1]) / 2
+        let z1 = (vertex2[2] + vertex3[2]) / 2
+
+        let x2 = (vertex1[0] + vertex3[0]) / 2
+        let y2 = (vertex1[1] + vertex3[1]) / 2
+        let z2 = (vertex1[2] + vertex3[2]) / 2
+
+        let x3 = (vertex1[0] + vertex2[0]) / 2
+        let y3 = (vertex1[1] + vertex2[1]) / 2
+        let z3 = (vertex1[2] + vertex2[2]) / 2
+
+        let ab = [x3, y3, z3]
+        let ac = [x2, y2, z2]
+        let bc = [x1, y1, z1]
+
+        step -= 1;
+
+        Sierpinski(vertex1, ac, ab, vertexArray, step)
+        Sierpinski(ab, bc, vertex2, vertexArray, step)
+        Sierpinski(vertex3, ac, bc, vertexArray, step)
+    }
+}
+
+
+function main()
+{
+    let canvas = document.getElementById("pyramidCanvas");
+    let glCtx = initWebGL(canvas);
+
+    initViewport(glCtx, canvas);
+    initGL(glCtx, canvas);
+
+    let pyramid = createPyramid(glCtx,  [0, 0, -3], [0, 1, 0]);
+
+    shaderProgram = initShader(glCtx, vertexShaderSource, fragmentShaderSource);
+    bindShaderAttributes(glCtx, shaderProgram);
+
+    update(glCtx, [pyramid]);
+}
+
+main();
